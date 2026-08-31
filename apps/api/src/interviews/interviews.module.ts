5991997d5c3c17fce9db62196e41424149a88696
sajadcut <sajadcut@gmail.com>
2026-09-01 00:56:01 +0330

    feat: register interview brain service

--- apps/api/src/interviews/interviews.module.ts
+++ apps/api/src/interviews/interviews.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
+import { InterviewBrainService } from "./interview-brain.service";
 import { InterviewsController } from "./interviews.controller";
 import { InterviewsService } from "./interviews.service";
 
 @Module({
   controllers: [InterviewsController],
-  providers: [InterviewsService],
-  exports: [InterviewsService],
+  providers: [InterviewsService, InterviewBrainService],
+  exports: [InterviewsService, InterviewBrainService],
 })
 export class InterviewsModule {}
