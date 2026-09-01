eda664494b35737d25588f9ead7144db10acc2d6
sajadcut <sajadcut@gmail.com>
2026-09-01 15:48:00 +0330

    feat(interviewer): add interviewer home

---
+++ apps/web/app/interviewer/page.tsx
@@ -0,0 +1,5 @@
+import { InterviewerList } from "../../components/interviewer/interviewer-list";
+
+export default function InterviewerHomePage() {
+  return <InterviewerList title="Today and upcoming" />;
+}
