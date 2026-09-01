bfcb1e3a849c0d10aae53da1a2b6f1f2742b5d81
sajadcut <sajadcut@gmail.com>
2026-09-02 01:16:21 +0330

    feat(storage): select configured storage provider

--- apps/api/src/storage/storage.module.ts
+++ apps/api/src/storage/storage.module.ts
@@ -1,13 +1,23 @@
 import { Global, Module } from "@nestjs/common";
+import { getEnv } from "../config/env";
 import { LocalFilesystemStorageAdapter } from "./local-filesystem-storage.adapter";
+import { S3CompatibleStorageAdapter } from "./s3-compatible-storage.adapter";
 import { StorageService } from "./storage.service";
-import { STORAGE_PROVIDER } from "./storage-provider";
+import { STORAGE_PROVIDER, type StorageProvider } from "./storage-provider";
 
 @Global()
 @Module({
   providers: [
     LocalFilesystemStorageAdapter,
-    { provide: STORAGE_PROVIDER, useExisting: LocalFilesystemStorageAdapter },
+    S3CompatibleStorageAdapter,
+    {
+      provide: STORAGE_PROVIDER,
+      inject: [LocalFilesystemStorageAdapter, S3CompatibleStorageAdapter],
+      useFactory: (
+        local: LocalFilesystemStorageAdapter,
+        s3: S3CompatibleStorageAdapter,
+      ): StorageProvider => (getEnv().STORAGE_PROVIDER === "s3" ? s3 : local),
+    },
     StorageService,
   ],
   exports: [STORAGE_PROVIDER, StorageService],
