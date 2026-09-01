40f22f331baf086867a760cc91084a3df4c47b25
sajadcut <sajadcut@gmail.com>
2026-09-01 15:51:53 +0330

    feat(interviewer): register interview assignment operations

--- apps/api/src/interviewer/interviewer.module.ts
+++ apps/api/src/interviewer/interviewer.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
+import { InterviewAssignmentAdminController } from "./interview-assignment-admin.controller";
+import { InterviewAssignmentAdminService } from "./interview-assignment-admin.service";
 import { InterviewerController } from "./interviewer.controller";
 import { InterviewerService } from "./interviewer.service";
 
 @Module({
-  controllers: [InterviewerController],
-  providers: [InterviewerService],
-  exports: [InterviewerService],
+  controllers: [InterviewAssignmentAdminController, InterviewerController],
+  providers: [InterviewAssignmentAdminService, InterviewerService],
+  exports: [InterviewAssignmentAdminService, InterviewerService],
 })
 export class InterviewerModule {}
