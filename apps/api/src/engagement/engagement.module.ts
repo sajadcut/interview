d2107744adcdc7b99647ce997f966bee0e50f338
sajadcut <sajadcut@gmail.com>
2026-09-01 17:48:23 +0330

    feat(engagement): register engagement closure services

--- apps/api/src/engagement/engagement.module.ts
+++ apps/api/src/engagement/engagement.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
+import { EngagementOperationsController } from "./engagement-operations.controller";
+import { EngagementOperationsService } from "./engagement-operations.service";
 import { EngagementController } from "./engagement.controller";
 import { EngagementService } from "./engagement.service";
 
 @Module({
-  controllers: [EngagementController],
-  providers: [EngagementService],
-  exports: [EngagementService],
+  controllers: [EngagementController, EngagementOperationsController],
+  providers: [EngagementService, EngagementOperationsService],
+  exports: [EngagementService, EngagementOperationsService],
 })
 export class EngagementModule {}
