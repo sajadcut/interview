0c319f1df30898b61573f3b570e2761e7f50a973
sajadcut <sajadcut@gmail.com>
2026-09-01 17:46:35 +0330

    feat(talent): register talent closure services

--- apps/api/src/sourcing/sourcing.module.ts
+++ apps/api/src/sourcing/sourcing.module.ts
@@ -2,10 +2,12 @@ import { Module } from "@nestjs/common";
 import { InternalTalentPoolAdapter } from "./internal-talent-pool.adapter";
 import { SourcingController } from "./sourcing.controller";
 import { SourcingService } from "./sourcing.service";
+import { TalentOperationsController } from "./talent-operations.controller";
+import { TalentOperationsService } from "./talent-operations.service";
 
 @Module({
-  controllers: [SourcingController],
-  providers: [InternalTalentPoolAdapter, SourcingService],
-  exports: [SourcingService],
+  controllers: [SourcingController, TalentOperationsController],
+  providers: [InternalTalentPoolAdapter, SourcingService, TalentOperationsService],
+  exports: [SourcingService, TalentOperationsService],
 })
 export class SourcingModule {}
