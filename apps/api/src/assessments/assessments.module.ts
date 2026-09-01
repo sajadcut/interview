ccf6cbdefc572137ecdf03a5cd63b67af8660b18
sajadcut <sajadcut@gmail.com>
2026-09-01 18:15:52 +0330

    feat(assessments): wire assessment closure services

--- apps/api/src/assessments/assessments.module.ts
+++ apps/api/src/assessments/assessments.module.ts
@@ -1,10 +1,15 @@
 import { Module } from "@nestjs/common";
+import {
+  AssessmentOperationsController,
+  CandidateAssessmentsController,
+} from "./assessment-operations.controller";
+import { AssessmentOperationsService } from "./assessment-operations.service";
 import { AssessmentsController } from "./assessments.controller";
 import { AssessmentsService } from "./assessments.service";
 
 @Module({
-  controllers: [AssessmentsController],
-  providers: [AssessmentsService],
-  exports: [AssessmentsService],
+  controllers: [AssessmentsController, AssessmentOperationsController, CandidateAssessmentsController],
+  providers: [AssessmentsService, AssessmentOperationsService],
+  exports: [AssessmentsService, AssessmentOperationsService],
 })
 export class AssessmentsModule {}
