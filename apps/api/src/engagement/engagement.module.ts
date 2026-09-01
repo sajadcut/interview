3258a44dc39a3ca937d8dedd4c34cc08c2c4b8ad
sajadcut <sajadcut@gmail.com>
2026-09-01 18:02:10 +0330

    feat(engagement): wire engagement workspace

--- apps/api/src/engagement/engagement.module.ts
+++ apps/api/src/engagement/engagement.module.ts
@@ -1,12 +1,18 @@
 import { Module } from "@nestjs/common";
 import { EngagementOperationsController } from "./engagement-operations.controller";
 import { EngagementOperationsService } from "./engagement-operations.service";
+import { EngagementWorkspaceController } from "./engagement-workspace.controller";
+import { EngagementWorkspaceService } from "./engagement-workspace.service";
 import { EngagementController } from "./engagement.controller";
 import { EngagementService } from "./engagement.service";
 
 @Module({
-  controllers: [EngagementController, EngagementOperationsController],
-  providers: [EngagementService, EngagementOperationsService],
-  exports: [EngagementService, EngagementOperationsService],
+  controllers: [
+    EngagementController,
+    EngagementOperationsController,
+    EngagementWorkspaceController,
+  ],
+  providers: [EngagementService, EngagementOperationsService, EngagementWorkspaceService],
+  exports: [EngagementService, EngagementOperationsService, EngagementWorkspaceService],
 })
 export class EngagementModule {}
