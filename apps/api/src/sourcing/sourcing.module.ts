180ad9ab20b36b5d46e173e8a51ca8b0f3e521a8
sajadcut <sajadcut@gmail.com>
2026-09-02 00:28:27 +0330

    feat(sourcing): register source adapter registry

--- apps/api/src/sourcing/sourcing.module.ts
+++ apps/api/src/sourcing/sourcing.module.ts
@@ -1,4 +1,5 @@
 import { Module } from "@nestjs/common";
+import { CandidateSourceRegistry } from "./candidate-source.registry";
 import { InternalTalentPoolAdapter } from "./internal-talent-pool.adapter";
 import { SourcingController } from "./sourcing.controller";
 import { SourcingService } from "./sourcing.service";
@@ -7,7 +8,12 @@ import { TalentOperationsService } from "./talent-operations.service";
 
 @Module({
   controllers: [SourcingController, TalentOperationsController],
-  providers: [InternalTalentPoolAdapter, SourcingService, TalentOperationsService],
-  exports: [SourcingService, TalentOperationsService],
+  providers: [
+    InternalTalentPoolAdapter,
+    CandidateSourceRegistry,
+    SourcingService,
+    TalentOperationsService,
+  ],
+  exports: [SourcingService, TalentOperationsService, CandidateSourceRegistry],
 })
 export class SourcingModule {}
