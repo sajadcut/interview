1dac9af8fb6c9a13733ade5478397b7ed572f73f
sajadcut <sajadcut@gmail.com>
2026-09-02 10:23:09 +0330

    feat(api): expose typed interview assignment options

--- apps/api/src/interviewer/interview-assignment-admin.controller.ts
+++ apps/api/src/interviewer/interview-assignment-admin.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOkResponse, ApiTags } from "@nestjs/swagger";
 import { Permissions } from "../auth/permissions";
 import { RequirePermissions } from "../auth/require-permissions.decorator";
+import { ApiStandardErrorResponses } from "../common/http/api-standard-error-responses.decorator";
 import { RequireTenant } from "../tenant/require-tenant.decorator";
+import { InterviewAssignmentOptionsDto } from "./interviewer.dto";
 import { InterviewAssignmentAdminService } from "./interview-assignment-admin.service";
 
 @ApiTags("interview-operations")
@@ -13,6 +15,8 @@ export class InterviewAssignmentAdminController {
   constructor(private readonly assignments: InterviewAssignmentAdminService) {}
 
   @Get("assignment-options")
+  @ApiOkResponse({ type: InterviewAssignmentOptionsDto })
+  @ApiStandardErrorResponses()
   getAssignmentOptions() {
     return this.assignments.getOptions();
   }
