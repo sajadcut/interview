756927163c833ae3bf328e3dd5a7e55071154874
sajadcut <sajadcut@gmail.com>
2026-09-05 22:53:29 +0330

    feat: surface realtime end-to-end interview harness

--- apps/web/app/app/interviews/internal-test/page.tsx
+++ apps/web/app/app/interviews/internal-test/page.tsx
@@ -1,12 +1,14 @@
 import { InternalInterviewHarness } from "../../../../components/interviews/internal-interview-harness";
 import { InternalLiveKitSessionHarness } from "../../../../components/interviews/internal-livekit-session-harness";
 import { InternalMediaReadinessPanel } from "../../../../components/interviews/internal-media-readiness-panel";
+import { InternalRealtimeInterviewHarness } from "../../../../components/interviews/internal-realtime-interview-harness";
 import { InternalSpeechLoopHarness } from "../../../../components/interviews/internal-speech-loop-harness";
 
 export default function InternalInterviewTestPage() {
   return (
     <div className="space-y-4">
       <InternalMediaReadinessPanel />
+      <InternalRealtimeInterviewHarness />
       <InternalLiveKitSessionHarness />
       <InternalSpeechLoopHarness />
       <InternalInterviewHarness />
